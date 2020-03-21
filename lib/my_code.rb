84ffde0278a94ce43c49e97ceb690d5a6195ff6d
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-21 20:24:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,9 +2,9 @@ def map(array)
   #map returns a new array of manipulated elements
   new = []
   i = 0 
-  
   while i < array.length 
-end
-
-  
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new
 end
