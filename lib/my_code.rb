9b2f1f7b75894db9886802480a11fa30b7aaf7f6
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-21 20:34:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,15 @@ def map(array)
 end
 
 def reduce(array, sv=nil)
+  if sv 
+    sum = sv 
+    i = 0 
+  else 
+    num1 = array[0]
+    i = 1 
+  end
+  
+  while i < array.length 
+    yield(sum, array[i])
   
 end
